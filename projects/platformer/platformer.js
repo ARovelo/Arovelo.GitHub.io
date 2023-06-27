3d41f1c2f6be888875565f0f364e36fa1a43c572
Alessandro Rovelo <alessandrorovelo@gmail.com>
2023-06-27 03:54:25 +0000

    <platformer>

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -46,18 +46,27 @@ $(function () {
     // TODO 1
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
-    // example usage: createPlatform(x,y,width,height)
-
-
-
-    
+    // example usage: createPlatform(x,y,width,height)\
+    // TODO 1
+// Create platforms
+createPlatform(100, 850, 200, 10); // Platform 1
+  createPlatform(200, 750, 200, 10); // Platform 2
+  createPlatform(500, 650, 200, 10); // Platform 3
+  createPlatform(700, 550, 200, 10); // Platform 4
+  createPlatform(350, 450, 200, 10); // Platform 5
+  createPlatform(600, 345, 100, 10); // Platform 6
+  createPlatform(900, 345, 200, 10); // Platform 7
+  createPlatform(700, 250, 200, 10); // Platform 8
+  createPlatform(1000, 150, 200, 10); // Platform 9
     // TODO 2
     // Create collectables
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-
-
+    createCollectable("grace", 590, 235); // Collectible near platform 6
+    createCollectable("max", 950, 455); // Collectible near Platform 2
+    createCollectable("database", 550, 625); // Collectible near Platform 3
+  
 
 
     // TODO 3
@@ -65,7 +74,10 @@ $(function () {
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
-
+    createCannon("left", 200, 1500); // Cannon on the left wall, positioned at 200px down, shooting every 1.5 seconds
+    createCannon("right", 850, 2000); // Cannon on the right wall, positioned at 850px down, shooting every 2 seconds
+    createCannon("top", 500, 3000); // Cannon on the top wall, positioned at 500px across, shooting every 3 seconds
+    createCannon("bottom", 700, 2500); // Cannon on the bottom wall, positioned at 700px across, shooting every 2.5 seconds
 
 
 
